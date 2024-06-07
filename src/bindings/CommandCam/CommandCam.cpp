4213df58e6dd7832fc777e5625c26cb84bd672d1
Mirn <sj21dsl@gmail.com>
2024-06-07 20:09:03 +0900

    add new camera type detection

--- src/bindings/CommandCam/CommandCam.cpp
+++ src/bindings/CommandCam/CommandCam.cpp
@@ -336,6 +336,8 @@ int main(int argc, char **argv)
 	hr = pPropBag->Read(L"FriendlyName", &var, 0);
 	fprintf(stderr, "Capture device: %ls\n", var.bstrVal);
 	VariantClear(&var);
+	bool new_camera_bugfix = (wcscmp(var.bstrVal, L"HD USB Camera") == 0);
+	fprintf(stderr, "new_camera_bugfix: %s\n", new_camera_bugfix ? "YES" : "no");
 
 	// Create capture filter and add to graph
 	hr = pMoniker->BindToObject(0, 0,
